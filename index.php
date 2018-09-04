b846d89509534dde956b5af840e54810f836b60e
mayank patel <mayank.patel104@gmail.com>
2018-09-04 14:47:19 +0530

    commited

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Google Plus feeds</title>
+        <link rel="stylesheet" href="dist/jquery.kyco.googleplusfeed2.css">
+        <link rel="stylesheet" href="css/jquery.kyco.googleplusfeed.demo.css">
+    </head>
+    <body>
+        <div class="gplus"></div>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="dist/jquery.kyco.googleplusfeed2.js"></script>
+        <script>
+            $(function () {
+                $('.gplus').kycoGooglePlusFeed2('104496883882693094679');
+            });
+        </script>
+    </body>
+</html>
